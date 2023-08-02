93bd80826549b2aca86d93d09f8b268dc73f0cd4
John <jyakou@gmx.fr>
2023-08-02 18:26:52 +0200

    Gravity - 08/08/2023 - 18h26

--- js/classes/Player.js
+++ js/classes/Player.js
@@ -6,6 +6,12 @@ class Player{
             y: 100,
         }
 
+        // gravity
+        this.velocity = {
+            x: 0,
+            y: 0,
+        }
+
         this.width = 100;
         this.height = 100;
         // en rapport avec update()
@@ -20,10 +26,12 @@ class Player{
     }
 
     update(){
+        // gravity
+        this.position.y += this.velocity.y;
         // pour empécher le carré de traverser l'écran
-        if(this.sides.bottom < canvas.height){
-            this.position.y++;
+        if(this.sides.bottom + this.velocity.y < canvas.height){
+            this.velocity.y += 1;
             this.sides.bottom = this.position.y + this.height;
-        }
+        }else this.velocity.y = 0;
     }
 }
